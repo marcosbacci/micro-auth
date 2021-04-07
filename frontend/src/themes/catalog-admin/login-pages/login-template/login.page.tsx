8db03f254837a2c8f39efaeab82082273538b18e
Marcos Bacci <marcosfbacci@gmail.com>
2021-04-06 23:31:10 -0300

    Alterando template

---
+++ frontend/src/themes/catalog-admin/login-pages/login-template/login.page.tsx
@@ -0,0 +1,12 @@
+// @flow 
+import * as React from 'react';
+type Props = {
+    
+};
+export const login.page = (props: Props) => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
